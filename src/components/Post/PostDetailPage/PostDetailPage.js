f28038f62db8133137154863b99be45d6946a391
GangHyeonLee <isaac1119@naver.com>
2022-11-04 14:49:40 +0900

    2022.11.04. 이강현 수정 - PostDetailPage : data(userId, Image, content...) 송수신 완료

--- src/components/Post/PostDetailPage/PostDetailPage.js
+++ src/components/Post/PostDetailPage/PostDetailPage.js
@@ -16,6 +16,9 @@ import "./PostDetailPage.css";
  * 작업일         작업자    작업내용
  * -------------------------------------------------------------
  * 2022.10.13    김영일    최초작성
+ * 2022.10.26    김영일    ui fixed
+ * 
+ * 2022.11.04    이강현    백엔드연결, 데이터바인딩 완료
  * -------------------------------------------------------------
  */
 
@@ -32,7 +35,7 @@ export default function PostDetailPage() {
     //     console.log(data);
     //    })
 
-    const inputs = { postId: "7" };
+    const inputs = { postId: "13" };
 
     commonAxios(`/post/postDetail`, inputs, callback);
 
@@ -44,78 +47,100 @@ export default function PostDetailPage() {
     return () => {};
   }, []);
 
+  if (loading) 
+  return(
+   <div className="box" style={{margin: "30px 0"}} >
+      {" "}
+      Loading... 
+    </div>
+  );
+
   if (postList.length === 0) {
   } else {
     return (
       <>
-        <div className="box" style={{ margin: "30px 0" }}>
-          <header className="header-post">
-            <div className="infos-post">
-              <img
-                className="img-header-post"
-                src="https://github.com/maykbrito.png"
-                alt="profile"
-              />
-              <p>{postList.postInfo.userId}</p>
-            </div>
-            <FiMoreHorizontal />
-          </header>
-          <div className="img-post">
-            <img
-              className="img-header-post"
-              src={`${postList.fileInfo[0].uuidFileNm}`}
-              alt="post"
-            />
-            {console.log(postList.fileInfo[0].uuidFileNm)}
-          </div>
-
-          <img src={`'/${postList.fileInfo[0].uuidFileNm}'`}></img>
-
-          <div className="footer-post">
-            <IconContext.Provider value={{ size: "30px" }}>
-              <section className="engagement-post">
-                <div className="icons-1">
-                  <div className="icon">
-                    <IoMdHeartEmpty />
+            <div className="post-detail-box">
+              <div className="post-detail-box-inner">
+                <div className="icon-arrow">
+                  <IoIosArrowBack />
+                </div>
+                <div className="post-detail-box-inner-inner">
+                  <div className="img-post">
+                    <img
+                      className="img-header-post"
+                      src={`${postList.fileInfo[0].uuidFileNm}`} 
+                      alt="post"
+                    />
+                  </div>
+                  <div className="post-detail-footer-post">
+                    <div className="post-detail-username">
+                      <div className="infos-post">
+                      <img
+                        className="img-header-post"
+                        src={`${postList.fileInfo[0].uuidFileNm}`} 
+                        alt="post"
+                      />
+                  <p>{postList.postInfo.userId}</p>
+                </div>
+                <FiMoreHorizontal className="info-button" />
+              </div>
+              <div className="post-detail-contents">
+                <div className="infos-post">
+                  <img
+                    className="img-header-post"
+                    src={`${postList.fileInfo[0].uuidFileNm}`} 
+                    alt="post"
+                  />
+                  <p>{postList.postInfo.userId}</p>
+                </div>
+              </div>
+              <IconContext.Provider value={{size : "30px"}}>
+                <div className="post-detail-engagement-post">
+                  <div className="icons-1">
+                    <div className="icon">
+                      <IoMdHeartEmpty />
+                    </div>
+                    <div className="icon">
+                      <BsChat />
+                    </div>
+                    <div className="icon">
+                      <FiSend />
+                    </div>
                   </div>
                   <div className="icon">
-                    <BsChat />
-                  </div>
-                  <div className="icon">
-                    <FiSend />
+                    <BsBookmark />
                   </div>
                 </div>
-                <div className="icon">
-                  <BsBookmark />
+              </IconContext.Provider>
+              <div className="post-detail-legend">
+                  <p>
+                    <span>{postList.postInfo.userId}</span>
+                    {postList.postInfo.postContent}
+                  </p>
                 </div>
-              </section>
-            </IconContext.Provider>
-            <section className="like">
-              <span>61 curtidas</span>
-            </section>
-            <div className="legend">
-              <p>
-                <span>{postList.postInfo.userId}</span>
-                {postList.postInfo.postContent}
-              </p>
-            </div>
-            <div className="time-post">
-              <time>{postList.postInfo.createDt}</time>
-            </div>
-            <div className="comment">
-              <div className="fake-comment">
+                <div className="post-detail-time-post">
+                <time>{postList.postInfo.createDt}</time>
+              </div>
+              <div className="post-detail-comment">
+                <div className="post-detail-fake-comment">
                 <IconContext.Provider value={{ size: "25px" }}>
-                  <div className="icon">
-                    <BsEmojiSmile />
-                  </div>
+                      <div className="icon">
+                        <BsEmojiSmile />
+                      </div>
                 </IconContext.Provider>
                 <input placeholder="댓글달기..." />
+                </div>
+                <button>게시</button>
               </div>
-              <button>게시</button>
             </div>
           </div>
+
+          <div className="icon-arrow">
+            <IoIosArrowForward />
+          </div>
         </div>
-      </>
+      </div>
+    </>
     );
   }
 }
